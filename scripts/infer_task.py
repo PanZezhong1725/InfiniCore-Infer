048eec53ea3fe044d82c5bfbb1ce2cef0e1158bb
Pan Zezhong <panzezhong@qiyuanlab.com>
2025-06-20 19:12:16 +0800

    tmp

---
+++ scripts/infer_task.py
@@ -0,0 +1,18 @@
+import asyncio
+
+
+class InferTask:
+    def __init__(self, id, tokens, request):
+        self.id_ = id
+        self.tokens = tokens
+        self.request = request
+        self.output_queue = asyncio.Queue()
+        self._kv_cache_pool_item = None
+        self.pos = 0
+        self.finished_reason = None
+        
+    def bind_kv_cache(self, kv_cache_pool_item, pos):
+        self._kv_cache_pool_item = kv_cache_pool_item
+        self.pos = pos
+        self.tokens = self.tokens[pos:]
+
